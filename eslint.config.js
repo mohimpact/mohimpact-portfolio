6ed9d519f9f5a429af7ebdea5eb3f979b72249c6
Muhammad Umar Abubakar <muhammadumarabubakar363@gmail.com>
2026-05-24 22:53:38 +0100

    design: update project thumbnails on portfolio

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 'latest',
       globals: globals.browser,
@@ -22,13 +16,21 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
-
-    // ✅ CORRECT POSITION
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]|motion' }],
       'no-console': 'warn',
       'eqeqeq': 'error',
       'prefer-const': 'warn',
     },
   },
-])
+]
